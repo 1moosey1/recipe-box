bb954f3fc52a73ccc848c958ed8eca93b2daf0c4
Juan Gonzalez <jgonzalez498@gmail.com>
2018-01-22 01:35:18 -0800

    Added Scrollable and Recipe Components with basic placeholder styling

---
+++ src/components/Recipe.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Recipe = (props) => {
+
+    return (
+        <div className="recipe" onClick={props.handleSelect}>
+            {props.title}
+        </div>
+    );
+};
+
+Recipe.propTypes = {
+
+    title: PropTypes.string.isRequired,
+    handleSelect: PropTypes.func
+};
+
+export default Recipe;
---
+++ src/components/Scrollable.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Scrollable = (props) => {
+
+    return(
+        <div className="scrollable">
+            {props.children}
+        </div>
+    );
+};
+
+Scrollable.propTypes = {
+
+    children: PropTypes.oneOfType([
+
+        PropTypes.element,
+        PropTypes.arrayOf(PropTypes.element)
+    ])
+};
+
+export default Scrollable;
