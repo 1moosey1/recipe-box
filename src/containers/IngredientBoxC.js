ea94712f2d1d11cb6a9c81ad3fa60fd6832bb341
1moosey1 <jgonzalez498@gmail.com>
2018-02-04 20:34:04 -0500

    Altered IngredientBox Container to also pass in whether state is in editing mode or not

--- src/containers/IngredientBoxC.js
+++ src/containers/IngredientBoxC.js
@@ -1,8 +1,18 @@
 import { connect } from 'react-redux';
 import IngredientBox from '../components/IngredientBox';
 
-const mapStateToProps = state => (
-  { recipe: state.recipes[state.selectedRecipe] }
-);
+const mapStateToProps = (state) => {
+  let recipeToDisplay;
+  if (state.editing) {
+    recipeToDisplay = state.dirtyRecipe;
+  } else {
+    recipeToDisplay = state.recipes[state.selectedRecipe];
+  }
+
+  return {
+    recipe: recipeToDisplay,
+    editing: state.editing
+  };
+};
 
 export default connect(mapStateToProps)(IngredientBox);
